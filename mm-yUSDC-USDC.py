cd6619023984e1a2e84b1048e8c9888fec82e40a
John Wooten <jwooten31@gatech.edu>
2022-05-24 19:10:04 -0400

    Going to get rid of ID logic

--- mm-yUSDC-USDC.py
+++ mm-yUSDC-USDC.py
@@ -1,4 +1,5 @@
-from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder, TrustLineFlags
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder
+from decimal import Decimal, getcontext
 import requests, json, time, math
 from pprint import pprint
 
@@ -18,24 +19,68 @@ MAX_OFFER = 1.42
 SECRET = "SBTPLXTXJDMJOXFPYU2ANLZI2ARDPHFKPKK4MJFYVZVBLXYM5AIP3LPK"
 
 def main():
+  getcontext().prec = 7
   try:
     SECRET = sys.argv[1]
   except:
     print("Running without key. Usage: python3 mm-yUSDC-USDC.py $secret")
-  print("Starting yUSDC-USDC market making algorithm from price range {} to {}".format(MIN_BID, MAX_OFFER))
+  finally:
+    print("Starting yUSDC-USDC market making algorithm from price range {} to {}".format(MIN_BID, MAX_OFFER))
   while(True):
     #init 
-    USDCbuyOutstanding = ...
-    yUSDCsellOutstanding = ...
     
-    currSellSideAsk = ...
-    currSellSideOfferID = ...
+    requestAddress = "https://" + HORIZON_INST + "/accounts/" + BT_TREASURY
+    data = requests.get(requestAddress).json()
+    accountBalances = data["balances"]
+    
+    for balances in accountBalances:
+      if("asset_code" == "USDC"):
+        USDCbuyOutstanding = balances["selling_liabilities"]
+        USDCavailable = str(Decimal(balances["balance"] - Decimal(USDCbuyOutstanding)))
+      elif("asset_code" == "yUSDC"):
+        yUSDCsellOutstanding = balances["selling_liabilities"]
+        yUSDCavailable = str(Decimal(balances["balance"] - Decimal(USDCsellOutstanding)))
+
+    activeBuyOfferIDs = []
+    activeSellOfferIDs = []
+    
+    requestAddress = data["_links"]["offers"]["href"].replace("{?cursor,limit,order}", "?limit={}".format(MAX_SEARCH))
+    data = requests.get(requestAddress).json()
+    outstandingOffers = data["_embedded"]["records"]
+    while(outstandingOffers != []):
+      for offers in outstandingOffers:
+        if(offers["selling"]["asset_code"] == "yUSDC" and offers["buying"]["asset_code"] == "USDC"):
+          activeSellOfferIDs.append(offers["id"])
+        elif(offers["selling"]["asset_code"] == "USDC" and offers["buying"]["asset_code"] == "yUSDC"):
+          activeBuyOfferIDs.append(offers["id"])
+      # Go to next cursor
+      if(length(outstandingOffers) < 200):
+        break:
+      requestAddress = data["_links"]["next"]["href"].replace("\u0026", "&")
+      data = requests.get(requestAddress).json()
+      outstandingOffers = data["_embedded"]["records"]
     
     currBuySideBid = ...
     currBuySideOfferID = ...
     
-    USDCavailable = 
-    yUSDCavailable = 
+    if(offers["selling"]["asset_code"] == "yUSDC" and offers["buying"]["asset_code"] == "USDC"):
+          activeSellOffers[offers["price"]] = offers["amount"]
+        elif(offers["selling"]["asset_code"] == "USDC" and offers["buying"]["asset_code"] == "yUSDC"):
+          bidPrice = Decimal(1) / Decimal(offers["price"])
+          activeBuyOffers[str(bidPrice)] = offers["amount"]
+    
+    
+    requestAddress = "https://" + HORIZON_INST + "/order_book?selling_asset_type=credit_alphanum12&selling_asset_code=yUSDC&selling_asset_issuer=" + yUSDC_ISSUER + "&buying_asset_type=credit_alphanum4&buying_asset_code=USDC&buying_asset_issuer=" + USDC_ISSUER + "&limit=" + MAX_SEARCH
+    
+    data = requests.get(requestAddress).json()
+    
+    bidsFromStellar = data["bids"]
+    asksFromStellar = data["asks"]
+    
+    for bids in bidsFromStellar:
+    
+    
+    
     
     bidSide = {} # price, amount
     askSide = {} # price, amount
@@ -51,9 +96,9 @@ def main():
     # get lists of offers up to 1.42
     
     lowestMeaningfulOffer = MAX_OFFER;
-
+ 
     for offerPrices, amounts in askSide:
-
+ 
       if(roundAmount > MIN_MEANINGFUL_POS && offerPrices < lowestOfferOver1KvolIn5stroopRangeMinVal):
         # logic to adjust sell offer
     
@@ -92,3 +137,4 @@ def main():
     
     time.sleep(32)
 
+main()
