666b0bd53718742101c8cfa7c133cade5485a2bf
John Wooten <jwooten31@gatech.edu>
2022-05-24 16:56:59 -0400

    Create cancelAllOustandingOffers.py

---
+++ cancelAllOustandingOffers.py
@@ -0,0 +1,29 @@
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder, TrustLineFlags
+from datetime import datetime
+from decimal import Decimal
+from pprint import pprint
+import requests, json
+
+BT_ISSUER = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7"
+
+def main():
+  
+  
+  StellarBlockchainBalances = {}
+  requestAddress = "https://" + HORIZON_INST + "/accounts?asset=" + queryAsset + ":" + BT_ISSUER + "&limit=" + MAX_SEARCH
+  data = requests.get(requestAddress).json()
+  blockchainRecords = data["_embedded"]["records"]
+  while(blockchainRecords != []):
+    for accounts in blockchainRecords:
+      accountAddress = accounts["id"]
+      for balances in accounts["balances"]:
+        try:
+          if balances["asset_code"] == queryAsset and balances["asset_issuer"] == BT_ISSUER:
+            accountBalance = Decimal(balances["balance"])
+        except:
+          continue
+      StellarBlockchainBalances[accountAddress] = accountBalance
+    # Go to next cursor
+    requestAddress = data["_links"]["next"]["href"].replace("%3A", ":")
+    data = requests.get(requestAddress).json()
+    blockchainRecords = data["_embedded"]["records"]
