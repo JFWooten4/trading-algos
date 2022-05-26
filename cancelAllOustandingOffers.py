23ee89b499023ef57e7f1d92089cc9dbeaf13eb6
John Wooten <60260750+JFWooten4@users.noreply.github.com>
2022-05-26 09:03:57 -0400

    Update cancelAllOustandingOffers.py

--- cancelAllOustandingOffers.py
+++ cancelAllOustandingOffers.py
@@ -1,32 +1,49 @@
-from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder, TrustLineFlags
-from datetime import datetime
-from decimal import Decimal
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder
 from pprint import pprint
-import requests, json
+import requests, json, sys
 
-BT_ISSUER
-
-#todo
- = "GDRM3MK6KMHSYIT4E2AG2S2LWTDBJNYXE4H72C7YTTRWOWX5ZBECFWO7"
+BT_TREASURY = "GD2OUJ4QKAPESM2NVGREBZTLFJYMLPCGSUHZVRMTQMF5T34UODVHPRCY"
+HORIZON_INST = "horizon.stellar.org"
+MAX_SEARCH = "200"
+TXN_FEE_STROOPS = 5000
 
 def main():
-  
-  
-  StellarBlockchainBalances = {}
-  requestAddress = "https://" + HORIZON_INST + "/accounts?asset=" + queryAsset + ":" + BT_ISSUER + "&limit=" + MAX_SEARCH
+  try:
+    SECRET = sys.argv[1]
+  except:
+    print("Failed without key")
+    return -1
+  offerData = []
+  requestAddress = "https://" + HORIZON_INST + "/accounts/" + BT_TREASURY
   data = requests.get(requestAddress).json()
-  blockchainRecords = data["_embedded"]["records"]
-  while(blockchainRecords != []):
-    for accounts in blockchainRecords:
-      accountAddress = accounts["id"]
-      for balances in accounts["balances"]:
-        try:
-          if balances["asset_code"] == queryAsset and balances["asset_issuer"] == BT_ISSUER:
-            accountBalance = Decimal(balances["balance"])
-        except:
-          continue
-      StellarBlockchainBalances[accountAddress] = accountBalance
-    # Go to next cursor
-    requestAddress = data["_links"]["next"]["href"].replace("%3A", ":")
-    data = requests.get(requestAddress).json()
-    blockchainRecords = data["_embedded"]["records"]
+  requestAddress = data["_links"]["offers"]["href"].replace("{?cursor,limit,order}", "?limit={}".format(MAX_SEARCH))
+  data = requests.get(requestAddress).json()
+  outstandingOffers = data["_embedded"]["records"]
+  for offers in outstandingOffers:
+    offerData.append(int(offers['id']))
+    oD = offers["buying"] # offerBuyingAssetDict
+    offerData.append(Asset(oD["asset_code"], oD["asset_issuer"]))
+    oD = offers["selling"] # offerSellingAssetDict
+    offerData.append(Asset(oD["asset_code"], oD["asset_issuer"]))
+  
+  server = Server(horizon_url = "https://" + HORIZON_INST)
+  treasury = server.load_account(account_id = BT_TREASURY)
+  signing_keypair = Keypair.from_secret(SECRET)
+  transaction = TransactionBuilder(
+    source_account = treasury,
+    network_passphrase = Network.PUBLIC_NETWORK_PASSPHRASE,
+    base_fee = TXN_FEE_STROOPS,
+  )
+  for offerIDs, buyingAssets, sellingAssets in zip(*[iter(offerData)]*3):
+    transaction.append_manage_buy_offer_op(
+      selling = sellingAssets,
+      buying = buyingAssets,
+      amount = "0",
+      price = "69",
+      offer_id = offerIDs,
+    )
+    transaction = transaction.set_timeout(30).build()
+    transaction.sign(signing_keypair)
+    server.submit_transaction(transaction)
+    print("Successfully cancelled {} offers".format(len(offerData/3)))
+main()
